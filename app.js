25ab727ca8cc8f150f9f5ef4069362766549bfe8
Deena <blonadine@gmail.com>
2025-03-23 21:52:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ function get_widgets(SETTINGS) {
         collapsible: true,
         attribute: 'categories',
         operator: 'and',
-        showMore: false,
+        showMore: true,
       }
     ),
     "refine_mechanics": panel('Mechanics')(instantsearch.widgets.refinementList)(
